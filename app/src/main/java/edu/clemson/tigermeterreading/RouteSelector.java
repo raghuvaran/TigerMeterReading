291348fef073ce19d9b14c024e02ed26bebcdf24
rchowda <ch.raghuvaran@gmail.com>
2016-07-13 11:51:27 -0400

    Initial commit

---
+++ app/src/main/java/edu/clemson/tigermeterreading/RouteSelector.java
@@ -0,0 +1,13 @@
+package edu.clemson.tigermeterreading;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RouteSelector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_selector);
+    }
+}
